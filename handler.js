6f37181c598416d0313c12b87477fe5850ef9d8e
Gamer Omega <76910135+gamer0mega@users.noreply.github.com>
2021-12-15 22:00:02 +0300

    Add Comments to Version 0.0.1

--- handler.js
+++ handler.js
@@ -1,18 +1,24 @@
+// Import FS
 const fs = require('fs');
+// Await The Request Message.
 process.on('message', function handleMessage(data) {
+    // Try To Execute The Method.
     try {
         const output = fs[data.action + 'Sync'](...data.args);
+        // If it Succeeded, Send The Success Message to The Parent.
         return process.send({
             event: 'data',
             payload: output
         });
     } catch(error) {
+        // If it Failed(Missing Permissions, ENOENT, etc.), Send The Failure Message to The Parent.
         return process.send({
             event: 'error',
             payload: error.toString()
         });
     }
 });
+// Monitor Failures Just in Case.
 process.on('uncaughtException', function fsChildOnUncaughtException(error) {
     return process.send({
         event: 'error',
--- improved-fs.js
+++ improved-fs.js
@@ -1,26 +1,38 @@
+// Import The Modules.
 const { fork } = require('child_process');
 const fs = require('fs');
+// The Base Execute Method.
 module.exports.execute = function improvedFS(action, ...args) {
+    // Check if The Method is invalid.
     if(!action || !fs[action + 'Sync']) throw new RangeError('Invalid Function');
     return new Promise(function awaitFSChild(resolve, reject) {
+        // Spawns a Fork For Performing The Method.
         const fsChild = fork(__dirname + '/handler.js');
+        // Receive Output From The Fork.
         fsChild.on('message', function fsChildHandlerOnMessage(data) {
+            // Check in case if The Response is Invalid.
             if(!data || !data.event) return;
+            // Use Switch-Case to Detect The Output Type.
             switch(data.event) {
                 case "error":
+                    // Rejects The Promise if There was an Error.
                     fsChild.kill();
                     reject(new Error(data.payload.substring('Error: '.length)));
                     break;
                 case "data":
+                    // Resolves The Promise if The Data was Successfully Received.
                     fsChild.kill();
+                    // If The Output is a JSON-Encoded Buffer, Parse it Back to a Buffer.
                     if(data.payload && data.payload.type === 'Buffer') data.payload = Buffer.from(data.payload);
                     resolve(data.payload);
                     break;
             };
         });
+        // Perform The Request.
         fsChild.send({action, args});
     });
 };
+// Import Methods from The Default NodeJS FS.
 for(const action of Object.keys(fs).filter(key => key.endsWith('Sync') && key != 'executeSync').map(key => key.substring(0, key.length - 'Sync'.length))) {
     module.exports[action] = function executeAsyncfsFunction(...args) {
         return module.exports.execute(action, ...args);
